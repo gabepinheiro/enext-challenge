df984f4f9491c2c28573b541f670441159cc8336
Gabriel Pinheiro <gabrielpsilvas@live.com>
2020-02-01 21:00:09 -0300

    Added functions que injeta options nas tag select

---
+++ src/js/components.js
@@ -0,0 +1,57 @@
+export const selectBreeds = listBreed => {
+  const markup = listBreed.map(item => {
+    const option = `<option value="${item}">${item}</option>`;
+    return option;
+  });
+
+  // elSelect.innerHTML = `<optgroup>${markup}</optgroup>`;
+  return markup;
+};
+
+export const selectFonts = () => {
+  const markup = [
+    "'Nunito Sans', sans-serif",
+    "'Bangers', sans-serif",
+    "'Permanent Marker', sans-serif",
+    "'Oleo Script', sans-serif",
+    "'Lobster', sans-serif",
+  ].map((font, index) => `<option value="${font}">Font ${index + 1}</option>`);
+
+  // elSelect.innerHTML = `<optgroup>${markup}</optgroup>`;
+  return markup;
+};
+
+export const selectColors = () => {
+  const colorsClass = [
+    {
+      class: 'colorFirst',
+      bgc: 'background: var(--color-first);',
+    },
+    {
+      class: 'colorSecond',
+      bgc: 'background: var(--color-second);',
+    },
+    {
+      class: 'colorThird',
+      bgc: 'background: var(--color-third);',
+    },
+    {
+      class: 'colorFourth',
+      bgc: 'background: var(--color-fourth);',
+    },
+    {
+      class: 'colorFifth',
+      bgc: 'background: var(--color-fifth);',
+    },
+  ];
+
+  const markup = colorsClass.map(
+    (item, index) =>
+      `<option style="${item.bgc}" value="${item.class}"> 
+          Cor ${index + 1}
+      </option>`
+  );
+
+  // elSelect.innerHTML = `<optgroup>${markup}</optgroup>`;
+  return markup;
+};
